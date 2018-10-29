4a4d38fd550d06cc96ba0e3d4257d2155f9326f4
Bielokudrenko <dev3aleksey.b@alldigitalads.com>
2018-10-29 13:28:26 +0200

    update runners-update

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-cd ~/.runner-scripts/ && git reset --hard  && git pull && ./init
+cd ~/.runner-scripts/ && git reset --hard  && git pull
+
+sudo chmod +x ~/.runner-scripts/run
+sudo chmod +x ~/.runner-scripts/update.sh
+sudo chmod +x ~/.runner-scripts/unistall.sh
+sudo chmod +x ~/.runner-scripts/init
+
+~/.runner-scripts/init
