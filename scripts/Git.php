9156f554ff9cb9d84f78580a2ade108fde9327a7
Bielokudrenko <dev3aleksey.b@alldigitalads.com>
2018-10-29 12:39:03 +0200

    add script git

---
+++ scripts/Git.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: noname
+ * Date: 29.10.18
+ * Time: 12:35
+ */
+
+namespace scripts;
+
+/**
+ * Class Git
+ * @package scripts
+ */
+class Git extends Scripts
+{
+    /**
+     * @return array
+     */
+    public function getListScripts()
+    {
+        return [
+            'pull' => ['field' => 'git pull', 'script' =>"git pull"],
+            'push' => ['field' => 'git push', 'script' =>"git push"],
+            'status' => ['field' => 'git status', 'script' =>"git status"],
+        ];
+    }
+}
